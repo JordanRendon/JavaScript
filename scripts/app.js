98050cf8f5b2415f106f1fef8a6d40e7f0a10c81
JordanRendon <jordanre7991@gmail.com>
2022-07-18 09:26:24 -0500

    creamos una constante y vimos variable underfined

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+let nombre=prompt("ingrese su nombre")
+let apellido=prompt("ingrese su apellido")
+
+console.log(nombre+" "+apellido)
+
+const email= "jordanre7991@gmail.com"
+console.log(`el correo es: ${email}`)
+
+let correo;
+console.log(`el correo es : ${correo}`);
