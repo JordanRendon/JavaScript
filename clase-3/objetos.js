ec65ba40ebf09fdf02fa4ab8c26810302ee4f843
JordanRendon <jordanre7991@gmail.com>
2022-07-21 10:04:07 -0500

    creacion de objetos

---
+++ clase-3/objetos.js
@@ -0,0 +1,32 @@
+//crear objetos 
+let objeto={}
+console.log(objeto)
+// creando obejto carro
+
+let carro={
+    color:'rojo',
+    velocidadMaxima:"100 km/h",
+    modelo:2020,
+    placa:'ABC123',
+    propietario:['Jordnan Rendon'],
+    lugarCreacion:{
+    ciudad:'Medellin',
+    pais:'Colombia'
+    },
+    tieneSoat:true
+}
+ console.log(carro)
+
+ let computador={
+    color:'negro',
+    sistemaOperativo:"Windows 10",
+    modelo:'lenovo',
+    placa:'SW120TX',
+    propietario:['Jordan Rendon'],
+    lugarCreacion:{
+    ciudad:'Medellin',
+    pais:'Colombia'
+    },
+    
+}
+ console.log(computador)
