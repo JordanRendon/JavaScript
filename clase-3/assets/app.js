474c8ab92bf623220364d2bc62112430ac65a717
JordanRendon <jordanre7991@gmail.com>
2022-07-21 08:51:29 -0500

    creacion de arreglos en JS

---
+++ clase-3/assets/app.js
@@ -0,0 +1,7 @@
+//array vacio
+let array=[];
+console.log(array.length);
+//crea,ps cuatro vectores con diferentes valores
+ let numeros=[1,2,3,4]
+ let comida=["pizza","salchipapas","pollo","arroz chino"]
+ let colores=["amarillo","rojo","azul","negro"]
