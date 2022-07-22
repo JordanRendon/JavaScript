ba8f739a721594ac69977e1b43c38f67045e9c12
JordanRendon <jordanre7991@gmail.com>
2022-07-22 09:31:21 -0500

    Creacion de funciones-nativas

---
+++ clase-4/assets/funciones-nativas.js
@@ -0,0 +1,22 @@
+let saludo='hola jordan'
+
+console.log(saludo)
+console.log(saludo.length) //devuelve la cantidad de caracteres
+
+console.log(saludo.toUpperCase())//devuelve el texto en mayusculas
+
+console.log(saludo.toLowerCase())//devuelve el texto en minisculas
+
+console.log(saludo.slice(0,4))
+console.log(saludo.slice(5,11))//devuelvo fragmentos compredidos entre las posiciones proporcionada por parametros
+
+console.log(saludo.includes('mundo'))//devuelve verdadero o falso si la cadena existe
+
+console.log(saludo.indexOf('a'))//devuelve la primera posicion en la que aprezca la cadena ingresada por pareametro
+
+console.log(saludo.replace('a', '@'))//permite remplazar una cadena de texto por otra
+
+let numero = 5
+
+console.log(typeof numero.toString())
+console.log(typeof numero)
