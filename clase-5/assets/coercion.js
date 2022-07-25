d9b1a6cdd0d97a78e43e25c014f5768faa49365b
JordanRendon <jordanre7991@gmail.com>
2022-07-25 09:18:56 -0500

    Vimos coercion de varios tipos de datos

---
+++ clase-5/assets/coercion.js
@@ -0,0 +1,17 @@
+let numero1 = 2, numero2 = '3'
+
+let resultado = numero1+numero2
+
+console.log(resultado)
+console.log(typeof resultado)
+
+let resultadoEntero = numero1 + Number(numero2);
+
+console.log(resultadoEntero)
+
+let nota1=3.5, nota2='4.8'
+
+let suma=nota1+parseFloat(nota2)
+ console.log(suma)
+
+console.log(typeof suma.toString()) 
