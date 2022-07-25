6e5686b43ac8ea31be5cda8ad224bd29925947fb
JordanRendon <jordanre7991@gmail.com>
2022-07-25 09:03:56 -0500

    vimos booleanos y operadores de igualdad

---
+++ clase-5/assets/app.js
@@ -0,0 +1,11 @@
+let isHungry=true;
+let isSunny=false;
+
+console.log(isHungry);
+console.log(isSunny);
+
+
+let numero1 = 5, numero2 = '5';
+
+console.log(numero1==numero2)
+console.log(numero1===numero2)
