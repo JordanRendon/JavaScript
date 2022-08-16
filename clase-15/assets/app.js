7c48755c1171d3c2c3cf950dac1eabe92888a1c9
JordanRendon <jordanre7991@gmail.com>
2022-08-16 09:21:14 -0500

    convertir objeto a JSON

---
+++ clase-15/assets/app.js
@@ -0,0 +1,12 @@
+const usuario = {
+    nombreCompleto: 'Jordan Stiven Rendón',
+    edad: 25,
+    cedula: 1152216126,
+    habilidades: ['Html','Css','JavaS'],
+}
+
+console.log(usuario)
+
+const usuarioJson = JSON.stringify(usuario)
+
+console.log(usuarioJson)
